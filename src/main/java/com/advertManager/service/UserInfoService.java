a3c708b2fe6f1e29edf36d05c0ae199835bd14b7
Midas Oleg <midas.oleg@gmail.com>
2017-11-18 18:12:31 +0200

    up

--- src/main/java/com/advertManager/service/UserInfoService.java
+++ src/main/java/com/advertManager/service/UserInfoService.java
@@ -4,9 +4,10 @@ package com.advertManager.service;
 import com.advertManager.dao.IUserInfoDAO;
 import com.advertManager.entity.Advert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserInfoService implements IUserInfoService{
 
     @Autowired
